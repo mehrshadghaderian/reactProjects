325eb6058ffc2c749d54090606e1711675d25851
amir <amir.nooroz.mobarak@gmail.com>
2022-11-14 14:32:01 +0330

    new

--- vidlyMosh/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "./index.css"
-//first problem index.css dont run
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
