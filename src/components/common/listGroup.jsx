e8ad92bd530c82c298d7df47db3f622b470c7c36
amir <amir.nooroz.mobarak@gmail.com>
2022-11-15 12:34:00 +0330

    react

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,18 +1,27 @@
 const ListGroup = (props) => {
-  const { items, textProperty, valueProperty } = props;
+  const { items, textProperty, valueProperty, onItemSelected, selectedItem } =
+    props;
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <li key={item[valueProperty]} className="list-group-item">
+        <li
+          onClick={() => onItemSelected(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem
+              ? "list-group-item active"
+              : "list-group-item "
+          }
+        >
           {item[textProperty]}
         </li>
       ))}
     </ul>
   );
 };
-ListGroup.defaultProps={
-  textProperty:"name",
-  valueProperty:"_id"
-}
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id",
+};
 
 export default ListGroup;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import ReactDOM from "react-dom/client";
-import "./index.css"
-//first problem index.css dont run
+import React from "react";
+import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+
+
+
+
+
+
+
+
+
+
